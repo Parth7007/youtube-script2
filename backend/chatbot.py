45f542967c50c52951b77386db9103708b8649ab
Mayuresh Bairagi <139955524+Mayuresh-Bairagi@users.noreply.github.com>
2024-10-04 11:16:28 +0530

    modifiy genai_model

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -4,3 +4,12 @@ import google.generativeai as genai
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model = genai.GenerativeModel("gemini-pro")
+
+chat = model.start_chat()
+system_prompt = "You are an expert in speech theory, providing clear, concise, and insightful explanations. Always deliver responses in a friendly tone, using simple language to ensure understanding."
+chat.send_message(system_prompt)
+chat_history = []
+chat_history = [f'System: {system_prompt}']
+
--- backend/genai_model.py
+++ backend/genai_model.py
@@ -1,4 +1,7 @@
 import os
+import re
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from dotenv import load_dotenv
 import google.generativeai as genai
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -13,9 +16,30 @@ class YouTubeSummary:
         and summarizing the entire video and providing the important summary in points
         within 250 words. Please provide the summary of the text given here:  """
 
+
+    def extract_youtube_key(self, url):
+        try:
+            patterns = [
+                r'(?:https?://)?(?:www\.)?youtube\.com/watch\?v=([a-zA-Z0-9_-]{11})',
+                r'(?:https?://)?(?:www\.)?youtube\.com/live/([a-zA-Z0-9_-]{11})',
+                r'(?:https?://)?(?:www\.)?youtu\.be/([a-zA-Z0-9_-]{11})'
+            ]
+
+            for pattern in patterns:
+                match = re.match(pattern, url)
+                if match:
+                    return match.group(1)
+            
+            raise ValueError("Invalid YouTube URL")
+        
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
+        
+    
+
     def extract_transcript_details(self, youtube_video_url):
         try:
-            video_id = youtube_video_url.split("=")[1]
+            video_id = youtube_video_url.split("v=")[-1]
             transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
             
             transcript = ""
